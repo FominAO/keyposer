16bcb2cc4727adcd85c81045a5220e26466ef1c1
FominAO <fomagoafterme@yandex.ru>
2019-02-09 13:49:20 +0300

    sw1

--- dist/keyposer/sw.js
+++
@@ -1,9 +0,0 @@
-!function () {
-  "use strict";
-  self.addEventListener("install", function (e) { 
-    return self.skipWaiting() 
-  }),
-  self.addEventListener("activate", function (e) { 
-    return self.clients.claim() 
-  })
-}
