98860c410c8f39f43bf77a075bf321e88af42c34
FominAO <fomagoafterme@yandex.ru>
2019-03-03 17:55:50 +0300

    --message

--- src/app/sw.service.ts
+++ src/app/sw.service.ts
@@ -3,18 +3,19 @@ import { Injectable, Output, EventEmitter, HostListener } from '@angular/core';
 export class PwaService {
   promptEvent;
   @Output() public getPrompt = new EventEmitter<any>();
-  @HostListener('window:beforeinstallprompt', ['$event'])
-  onbeforeinstallprompt(e) {
-    console.log(e);
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    this.getPrompt.emit(e);
-  }
+  // @HostListener('window:beforeinstallprompt', ['$event'])
+  // onbeforeinstallprompt(e) {
+  //   console.log(e);
+  //   // Prevent Chrome 67 and earlier from automatically showing the prompt
+  //   e.preventDefault();
+  //   // Stash the event so it can be triggered later.
+  //   this.getPrompt.emit(e);
+  // }
   constructor() {
-    // window.addEventListener('beforeinstallprompt', event => {
-    //   this.promptEvent = event;
-    // });
+    window.addEventListener('beforeinstallprompt', event => {
+      event.preventDefault();
+      this.promptEvent = event;
+    });
   }
   promptChecked() {
     this.getPrompt.emit(this.getPrompt);
